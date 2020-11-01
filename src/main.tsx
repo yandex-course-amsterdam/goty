541a2027fb81183e18233bcdd7a820b60bfdf776
Anton Subbotin <anton.subbotin@diginetica.com>
2020-11-01 15:41:22 +0300

    [GOTY-18] обновил роутер, добавил роутер в main.tsx, добавил правило endOfLine в .prettierrc

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import 'normalize.css'
 
 import { Game } from 'app'
 
-ReactDOM.render(<Game />, document.getElementById('root'))
+export const Main = (): ReactElement => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Game />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
