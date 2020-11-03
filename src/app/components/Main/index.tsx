e58f11ed67033391721cec9642229141b978064d
Igor Kirpinev <kirpinev@yahoo.com>
2020-11-03 12:17:16 +0300

    Обновил компонент Main, добавил условный рендеринг

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -6,6 +6,9 @@ type MainProps = {
   children: ReactElement[] | ReactElement
 }
 
-export const Main = ({ children }: MainProps): ReactElement => {
-  return <section className={style.main}>{children}</section>
-}
+export const Main = ({ children }: MainProps): ReactElement =>
+  Array.isArray(children) ? (
+    <section className={style.main}>{...children}</section>
+  ) : (
+    <section className={style.main}>{children}</section>
+  )
