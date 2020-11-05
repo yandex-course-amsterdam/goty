eec38b15eb8614282862cb706065d711247f53e1
Anton Subbotin <anton.subbotin@diginetica.com>
2020-11-05 14:50:31 +0300

    [GOTY-11] Добавил сущности и хелперы для компонента игры

---
+++ src/app/components/Canvas/entities/Board.ts
@@ -0,0 +1,21 @@
+import { BoardInterface } from './Types'
+import { Slot } from './Slot'
+
+export class Board implements BoardInterface {
+  slots: Slot[] = []
+
+  x = 0
+
+  y = 0
+
+  ctx
+
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx
+  }
+
+  draw(x: number, y: number): void {
+    this.x = x
+    this.y = y
+  }
+}
---
+++ src/app/components/Canvas/entities/Control.ts
@@ -0,0 +1,21 @@
+import { ControlInterface } from './Types'
+
+export class Control implements ControlInterface {
+  text = ''
+
+  x = 0
+
+  y = 0
+
+  ctx
+
+  constructor(text: string, ctx: CanvasRenderingContext2D) {
+    this.text = text
+    this.ctx = ctx
+  }
+
+  draw(x: number, y: number): void {
+    this.x = x
+    this.y = y
+  }
+}
