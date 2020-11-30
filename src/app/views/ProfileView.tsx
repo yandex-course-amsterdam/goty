cd66c53e6ed294ab9ee478630492e309aafea3fd
Igor Kirpinev <kirpinev@yahoo.com>
2020-11-30 20:56:03 +0300

    Подправил вьюхи

--- src/app/views/ProfileView.tsx
+++ src/app/views/ProfileView.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { FC } from 'react'
 import { Profile } from 'app/containers/Profile'
 
-export const ProfileView = (): ReactElement => <Profile />
+export const ProfileView: FC = (): JSX.Element => <Profile />
