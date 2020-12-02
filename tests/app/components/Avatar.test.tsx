0f8a8d1ff0e3f7f202fc19a45b7c64de5f909a17
Anton Subbotin <anton.subbotin@diginetica.com>
2020-12-02 22:54:45 +0300

    [GOTY-33] добавил первый тест

---
+++ tests/app/components/Avatar.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import * as renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+import { Avatar } from 'app/components/Avatar'
+
+let store
+
+test('Link changes the class when hovered', () => {
+  const initialState = {
+    userData: {
+      avatar: 'https://static.insider.com/image/5d24d6b921a861093e71fef3.jpg'
+    }
+  }
+  const mockStore = configureStore()
+  store = mockStore(initialState)
+  const component = renderer.create(
+    <Provider store={store}>
+      <Avatar />
+    </Provider>
+  )
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
