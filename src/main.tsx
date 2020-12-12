e69420d1a4221c5d9ddeb8bf9f48193d9bf67079
Anton Subbotin <anton.subbotin@diginetica.com>
2020-12-12 21:40:15 +0300

    [GOTY-40] добавил оповещение о потере соединения

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider, useDispatch } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { PrivateRoute } from 'app/components'
+import { CSSTransition } from 'react-transition-group'
+
 import {
   SignUpView,
   SignInView,
@@ -11,19 +12,21 @@ import {
   ScoreView,
   LoaderView
 } from 'app/views'
+import { OfflineBar, PrivateRoute } from 'app/components'
 
 import { store } from 'app/store'
 import { fetchUserInfo } from 'app/actions'
 import { route } from 'app/enums'
-import { startServiceWorker } from 'app/utils'
+// import { startServiceWorker } from 'app/utils'
 
 import 'normalize.css'
 import './fonts/fonts.css'
 
-startServiceWorker()
+// startServiceWorker()
 
 export const Main: FC = (): JSX.Element => {
   const [isLoading, setIsLoading] = useState(true)
+  const [isOffline, setIsOffline] = useState(false)
   const dispatch = useDispatch()
 
   const getUserData = async () => {
@@ -35,32 +38,49 @@ export const Main: FC = (): JSX.Element => {
     }
   }
 
+  const handleOfflineCallback = useCallback(() => setIsOffline(true), [])
+  const handleOnlineCallback = useCallback(() => setIsOffline(false), [])
+
+  window.addEventListener('offline', handleOfflineCallback)
+  window.addEventListener('online', handleOnlineCallback)
+
   useEffect(() => {
     getUserData()
+
+    return () => {
+      window.removeEventListener('offline', handleOfflineCallback)
+      window.removeEventListener('online', handleOnlineCallback)
+    }
   }, [])
 
   return isLoading ? (
     <LoaderView />
   ) : (
-    <Router>
-      <Switch>
-        <Route path={route.signUp}>
-          <SignUpView />
-        </Route>
-        <Route path={route.signIn}>
-          <SignInView />
-        </Route>
-        <PrivateRoute path={route.profile}>
-          <ProfileView />
-        </PrivateRoute>
-        <PrivateRoute exact path={route.game}>
-          <GameView />
-        </PrivateRoute>
-        <PrivateRoute path={route.score}>
-          <ScoreView />
-        </PrivateRoute>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route path={route.signUp}>
+            <SignUpView />
+          </Route>
+          <Route path={route.signIn}>
+            <SignInView />
+          </Route>
+          <PrivateRoute path={route.profile}>
+            <ProfileView />
+          </PrivateRoute>
+          <PrivateRoute exact path={route.game}>
+            <GameView />
+          </PrivateRoute>
+          <PrivateRoute path={route.score}>
+            <ScoreView />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+
+      <CSSTransition in={isOffline} timeout={0} classNames="bar">
+        <OfflineBar />
+      </CSSTransition>
+    </>
   )
 }
 
