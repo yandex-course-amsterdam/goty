2f1c5ea4338608466a4e212fee77a59237c26dff
Anton Subbotin <anton.subbotin@diginetica.com>
2020-12-15 00:21:31 +0300

    [GOTY-50] добавил работу с localStorage для хранения запросов в случае интернет ошибок

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,4 @@
 export { sanitize } from './sanitize'
 export { displayResponseText } from './displayResponseText'
 export { startServiceWorker } from './startServiceWorker'
+export * from './storage'
---
+++ src/app/utils/storage.ts
@@ -0,0 +1,20 @@
+export const ITEM_NAME = 'amsterdamScore'
+
+export const getScore = (): Record<string, any> | null => {
+  const scoreCurrent = localStorage.getItem(ITEM_NAME)
+  return scoreCurrent ? JSON.parse(scoreCurrent) : null
+}
+
+export const storeScore = (scoreNew: Record<string, any>): void => {
+  const scoreCurrent = getScore()
+  let scoreGreater = scoreNew
+
+  if (
+    scoreCurrent &&
+    scoreCurrent.data[ITEM_NAME] > scoreGreater.data[ITEM_NAME]
+  ) {
+    scoreGreater = scoreCurrent
+  }
+
+  localStorage.setItem(ITEM_NAME, JSON.stringify(scoreGreater))
+}
