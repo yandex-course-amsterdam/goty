ae40965f385e8f089f128b5fd4152020f2b2c751
Anton Subbotin <anton.subbotin@diginetica.com>
2021-01-24 10:58:14 +0300

    [GOTY-60] проаплейтил ридми, добавил условие для dotenv-плагина

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV === 'development') {
+  // В противном случае переменные окружения задаются с помощью docker-compose.yml
+  dotenv.config({ path: './config-local.env' })
+}
+
 require('./build/server')
