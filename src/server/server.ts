d4ac5fc60b682d3be6cf0ca7ad4aa3a926e43d6a
Anton Subbotin <anton.subbotin@diginetica.com>
2021-02-03 17:46:53 +0300

    [GOTY-61-repeat] добавил ssh-экшен

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,20 @@
+import fs from 'fs'
+import https from 'https'
+
 import { connectDB } from './db/connections'
 import { app } from './app'
+
+const credentials = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
+
+const server = https.createServer(credentials, app)
 
 const startServer = async () => {
   connectDB()
   const PORT = process.env.PORT || 5000
-  app.listen(process.env.PORT || PORT)
+  server.listen(process.env.PORT || PORT)
 }
 
 startServer()
