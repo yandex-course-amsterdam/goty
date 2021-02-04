52acb854c34b01a59a0af55c1608813dc165bc0e
Anton Subbotin <anton.subbotin@diginetica.com>
2021-02-04 14:29:16 +0300

    Доработал гитхаб-экшен

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,10 @@
-import fs from 'fs'
-import https from 'https'
-
 import { connectDB } from './db/connections'
 import { app } from './app'
-
-const credentials = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-}
-
-const server = https.createServer(credentials, app)
 
 const startServer = async () => {
   connectDB()
   const PORT = process.env.PORT || 5000
-  server.listen(process.env.PORT || PORT)
+  app.listen(process.env.PORT || PORT)
 }
 
 startServer()
